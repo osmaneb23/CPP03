ed6f3ba05cb2db35a03ebbf14edef618620226d6
Osmane Bouayed <obouayed@bess-f3r4s13.clusters.42paris.fr>
2025-03-31 11:26:08 +0200

    add/remove eof

--- ex02/src/ScavTrap.cpp
+++ ex02/src/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 17:20:06 by obouayed          #+#    #+#             */
-/*   Updated: 2025/03/29 17:41:54 by obouayed         ###   ########.fr       */
+/*   Updated: 2025/03/31 11:25:29 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ ScavTrap::ScavTrap() : ClapTrap()
     _hitpoints = 100;
     _energyPoints = 50;
     _attackDamage = 20;
+	_isGuarding = false;
     std::cout << "ScavTrap " << _name << " is created." << std::endl;
 }
 
@@ -25,22 +26,23 @@ ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
     _hitpoints = 100;
     _energyPoints = 50;
     _attackDamage = 20;
+	_isGuarding = false;
     std::cout << "ScavTrap " << _name << " is created." << std::endl;
 }
 
-ScavTrap::ScavTrap(ScavTrap const & src)
+ScavTrap::ScavTrap(ScavTrap const & src): ClapTrap(src)
 {
+	std::cout << "ScavTrap copy constructor called with source name: " << src._name << "." << std::endl;
     *this = src;
 }
 
 ScavTrap &ScavTrap::operator=(const ScavTrap &rhs)
 {
+	std::cout << "ScavTrap assignation operator called with right hand side name: " << rhs._name << "." << std::endl;
     if (this != &rhs)
     {
-        _name = rhs._name;
-        _hitpoints = rhs._hitpoints;
-        _energyPoints = rhs._energyPoints;
-        _attackDamage = rhs._attackDamage;
+        ClapTrap::operator=(rhs);
+        _isGuarding = rhs._isGuarding;
     }
     return (*this);
 }
@@ -52,7 +54,23 @@ ScavTrap::~ScavTrap()
 
 void ScavTrap::guardGate()
 {
-    std::cout << "ScavTrap " << _name << " is now in Gate keeper mode." << std::endl;
+    if (_hitpoints == 0)
+    {
+        std::cout << "ScavTrap " << _name << " is already dead and can't enter Gate keeper mode." << std::endl;
+        return;
+    }
+	if (_energyPoints == 0)
+	{
+		std::cout << "ScavTrap " << _name << " is out of energy and can't enter Gate keeper mode." << std::endl;
+		return;
+	}
+	if (_isGuarding)
+	{
+		std::cout << "ScavTrap " << _name << " is already in Gate keeper mode." << std::endl;
+		return;
+	}
+	std::cout << "ScavTrap " << _name << " has entered Gate keeper mode." << std::endl;
+    _isGuarding = true;
 }
 
 void ScavTrap::attack(const std::string &target)
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 16:30:48 by obouayed          #+#    #+#             */
-/*   Updated: 2025/03/29 18:17:14 by obouayed         ###   ########.fr       */
+/*   Updated: 2025/03/31 11:25:33 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,39 +20,58 @@ int main()
     ScavTrap defaultTrap;
     
     // Test parameterized constructor
-    ScavTrap scavtrap1("SC4V-TP");
+    ScavTrap scavTrap1("SC4V-TP");
     
     // Test copy constructor
-    ScavTrap scavtrap2(scavtrap1);
+    ScavTrap scavTrap2(scavTrap1);
     
     std::cout << "\n=== TESTING BASIC FUNCTIONALITY ===\n" << std::endl;
     
-    // Test attack function - note this uses ScavTrap's version
-    scavtrap1.attack("Bandit");
+    // Test attack function (ScavTrap version)
+    scavTrap1.attack("Bandit");
     
-    // Test takeDamage function - inherited from ClapTrap
-    scavtrap1.takeDamage(30);
+    // Test takeDamage function (inherited from ClapTrap)
+    scavTrap1.takeDamage(30);
     
-    // Test beRepaired function - inherited from ClapTrap
-    scavtrap1.beRepaired(15);
+    // Test beRepaired function (inherited from ClapTrap)
+    scavTrap1.beRepaired(15);
     
     // Test unique ScavTrap ability
-    scavtrap1.guardGate();
+    scavTrap1.guardGate();
+    scavTrap1.guardGate(); // Test calling guardGate twice
+    
+    std::cout << "\n=== TESTING COPY CONSTRUCTOR AND ASSIGNMENT ===\n" << std::endl;
+    
+    ScavTrap original("Original");
+    original.takeDamage(20);
+    original.attack("Target");
+    
+    std::cout << "\nCreating a copy with copy constructor:" << std::endl;
+    ScavTrap copy(original);
+    
+    std::cout << "\nTesting assignment operator:" << std::endl;
+    ScavTrap assignTarget("AssignTarget");
+    assignTarget = original;
     
     std::cout << "\n=== TESTING ENERGY DEPLETION ===\n" << std::endl;
     
+    // Create new ScavTrap for energy testing (50 energy points)
+    ScavTrap energyTest("EnergyTest");
+    
     // Attack multiple times to deplete energy
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < 51; i++) {
         if (i % 10 == 0)
             std::cout << "Attack attempt " << (i + 1) << ": ";
-        scavtrap2.attack("Target Dummy");
+        energyTest.attack("Target Dummy");
         if (i % 10 == 9)
             std::cout << std::endl;
     }
     
     // Try actions with no energy
-    std::cout << "\nAttempting to attack with no energy:" << std::endl;
-    scavtrap2.attack("Enemy");
+    std::cout << "\nAttempting actions with no energy:" << std::endl;
+    energyTest.attack("Enemy");
+    energyTest.beRepaired(10);
+    energyTest.guardGate();
     
     std::cout << "\n=== TESTING LETHAL DAMAGE ===\n" << std::endl;
     
@@ -60,7 +79,6 @@ int main()
     ScavTrap defender("Defender");
     
     // Deal a lot of damage
-    std::cout << "Dealing massive damage:" << std::endl;
     defender.takeDamage(150);
     
     // Try actions while dead
@@ -70,4 +88,3 @@ int main()
     
     return (0);
 }
-
